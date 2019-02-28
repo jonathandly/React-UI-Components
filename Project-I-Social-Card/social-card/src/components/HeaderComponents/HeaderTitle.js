0fadd8b583c6058ec8290223f72f2c0d88a44d29
Jonathan Daly <dalyj138@gmail.com>
2019-02-27 20:35:48 -0500

    Finished MVP components.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+
+
+function HeaderTitle() {
+    return (
+            <h1>Lambda School <span className="lamb-span">@LambdaSchool 26 jan</span></h1>
+    );
+}
+
+export default HeaderTitle;
